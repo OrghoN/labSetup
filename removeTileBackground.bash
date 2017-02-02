8dee5cc6f548aac729efda8a3623f82fd63d7c26
OrghoN <anoronyo@gmail.com>
2017-02-02 16:44:32 -0600

    edited tile remover to make copying and pasting commands easy

--- removeTileBackground.bash
+++ removeTileBackground.bash
@@ -1,15 +1,9 @@
 #!/bin/bash
-
-DIRECTORY=~/backup_icons_notile/
-
-echo "Remove Tile Backgrounds"
-if [[ ! -d "${DIRECTORY}" && ! -L "${DIRECTORY}" ]] ; then
-    wget -O ~/Downloads/icons.tar.gz https://www.dropbox.com/s/31ory2zycvk5c6x/icons.tar.gz
-    mkdir ~/backup_icons_notile
-    cp /usr/share/unity/icons/* ~/backup_icons_notile
-    cd /usr/share/unity
-    sudo tar zxvf ~/Downloads/icons.tar.gz
-    rm ~/Downloads/icons.tar.gz
-  else
-    echo "Backgrounds already removed"
-fi
+cd ~/Downloads
+wget https://www.dropbox.com/s/n48gz3g0fachi3t/icons.tar.gz
+mkdir ~/backup_icons
+cp /usr/share/unity/icons/* ~/backup_icons
+cd /usr/share/unity
+sudo tar zxvf ~/Downloads/icons.tar.gz
+cd ~/Downloads
+rm icons.tar.gz
